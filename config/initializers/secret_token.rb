a16186c4d061d12c17f9dc56a6d086257443f93d
Lane Aasen <laneaasen@gmail.com>
2012-08-25 18:41:45 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fireshop::Application.config.secret_token = '9489811834207be4f5210419b65855e4a2022be3c4ab7ef325beb122a4fb1a9bef586978f429d3541051f826341e120cfc391d32571299a540eecf0a74d720eb'
